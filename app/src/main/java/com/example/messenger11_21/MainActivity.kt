f93ba165789f4ee78267b171e4b122a87ff7b50d
Cailey <76137161+pseudonym17@users.noreply.github.com>
2021-11-16 11:42:02 -0700

    Changed contacts

--- app/src/main/java/com/example/messenger11_21/MainActivity.kt
+++ app/src/main/java/com/example/messenger11_21/MainActivity.kt
@@ -8,6 +8,10 @@ import android.view.ContextMenu
 import android.view.Menu
 import android.view.MenuItem
 import android.view.View
+import android.widget.EditText
+import android.widget.ImageView
+import android.widget.TextView
+import android.widget.Toast
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.google.firebase.auth.FirebaseAuth
@@ -16,9 +20,11 @@ import com.google.firebase.database.*
 class MainActivity : AppCompatActivity() {
     private lateinit var userRecyclerView: RecyclerView
     private lateinit var userList: ArrayList<User>
+    private lateinit var contactList: ArrayList<User>
     private lateinit var adapter: UserAdapter
     private lateinit var mAuth: FirebaseAuth
     private lateinit var db: DatabaseReference
+    private lateinit var addBtn: ImageView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -28,13 +34,17 @@ class MainActivity : AppCompatActivity() {
         db = FirebaseDatabase.getInstance().getReference()
 
         userList = ArrayList()
-        adapter = UserAdapter(this, userList)
+        contactList = ArrayList()
+
+        adapter = UserAdapter(this, contactList)
 
         userRecyclerView = findViewById(R.id.userRecyclerView)
 
         userRecyclerView.layoutManager = LinearLayoutManager(this)
         userRecyclerView.adapter = adapter
+        val uid = mAuth.currentUser?.uid!!
 
+        // Search for users of the app from Firebase
         db.child("user").addValueEventListener(object: ValueEventListener{
             override fun onDataChange(snapshot: DataSnapshot) {
                 userList.clear()
@@ -45,13 +55,39 @@ class MainActivity : AppCompatActivity() {
                         userList.add(currentUser!!)
                     }
                 }
+            }
+            override fun onCancelled(error: DatabaseError) {}
+        })
+
+        db.child("user").child(uid).child("contacts").addValueEventListener(object: ValueEventListener{
+            override fun onDataChange(snapshot: DataSnapshot) {
+                contactList.clear()
+                for (postSnapshot in snapshot.children) {
+                    val contact = postSnapshot.getValue(User::class.java)
+                    println(contact)
+                    Toast.makeText(this@MainActivity, "Contact: $contact", Toast.LENGTH_SHORT).show()
+                    contactList.add(contact!!)
+                }
                 adapter.notifyDataSetChanged()
             }
-
-            override fun onCancelled(error: DatabaseError) {
-                TODO("Not yet implemented")
-            }
+            override fun onCancelled(error: DatabaseError) {}
         })
+
+        addBtn = findViewById(R.id.addBtn)
+        addBtn.setOnClickListener{
+            val editContact = findViewById<EditText>(R.id.contact)
+            val newContact = editContact.text.toString()
+            db = FirebaseDatabase.getInstance().getReference()
+            // Add a new contact if they exist in userList
+            var verifiedContact : User
+            for (user in userList) {
+                if (user.name == newContact) {
+                    verifiedContact = user
+                    db.child("user").child(uid).child("contacts").child(newContact).setValue(verifiedContact)
+                    editContact.text.clear()
+                }
+            }
+        }
 
     }
 
